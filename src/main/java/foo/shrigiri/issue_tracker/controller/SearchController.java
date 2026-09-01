d79e974c57793c0cdea9baca9a62bccd3b43f427
shrigiri81 <darkg889@gmail.com>
2026-09-01 21:46:11 +0530

    feat: update SearchController with RESTful annotations for improved search functionality

--- src/main/java/foo/shrigiri/issue_tracker/controller/SearchController.java
+++ src/main/java/foo/shrigiri/issue_tracker/controller/SearchController.java
@@ -3,7 +3,10 @@ package foo.shrigiri.issue_tracker.controller;
 import foo.shrigiri.issue_tracker.service.SearchService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
